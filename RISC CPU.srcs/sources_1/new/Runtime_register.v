9302bffed968aba43456884a58af0386cb977da7
Singularity <1907677524gjk@gmail.com>
2023-09-11 09:09:37 +0800

    add test bench for runtime register

--- RISC CPU.srcs/sources_1/new/Runtime_register.v
+++ RISC CPU.srcs/sources_1/new/Runtime_register.v
@@ -21,6 +21,9 @@
 
 
 module Runtime_register(
+    input A_EN,
+    input B_EN,
+    input I_EN,
     input [1:0] Address_out_a,
     output reg [15:0] Data_out_a,
     input [1:0] Address_out_b,
@@ -30,4 +33,25 @@ module Runtime_register(
     );
 
     reg [15:0] General_register [3:0];
+
+    initial begin
+        General_register[0] <= 16'hzzzz;
+        General_register[1] <= 16'hzzzz;
+        General_register[2] <= 16'hzzzz;
+        General_register[3] <= 16'hzzzz;
+        Data_out_a <= 16'hzzzz;
+        Data_out_b <= 16'hzzzz;
+    end
+
+    always @(posedge A_EN) begin
+        Data_out_a <= General_register[Address_out_a];
+    end
+
+    always @(posedge B_EN) begin
+        Data_out_b <= General_register[Address_out_b];
+    end
+
+    always @(posedge I_EN) begin
+        General_register[Address_in] <= Data_in;
+    end
 endmodule
